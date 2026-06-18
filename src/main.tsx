8df4a8e53e9d1ac0cc1a4db603bd39c84341791c
casio_699 <kits.tech.co@gmail.com>
2026-06-18 08:30:06 +0300

    setting up sentry and testing with intentional error

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,32 @@
+import * as Sentry from "@sentry/react";
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import './tailwind.css';
 import { log } from './utils/logger';
+
+Sentry.init({
+  dsn: "https://c72181c8ef7291c6dc07a51c7af3ea6e@o4511584735854592.ingest.de.sentry.io/4511584748699728",
+  dataCollection: {
+    // To disable sending user data and HTTP bodies, uncomment the lines below. For more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/react/configuration/options/#dataCollection
+    // userInfo: false,
+    // httpBodies: []
+  }
+});
+
+// Add this button component to your app to test Sentry's error tracking
+function ErrorButton() {
+  return (
+    <button
+      onClick={() => {
+        throw new Error('This is your first error!');
+      }}
+    >
+      Break the world
+    </button>
+  );
+}
 
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
